9b3c2c02f1588c10cdf0911e0071804eb3a99bf3
jenniesaf <janny.janny@gmail.com>
2024-04-17 18:05:08 +0200

    clear unused method

--- clothing-store/src/routes/sign-in/sign-in.component.jsx
+++ clothing-store/src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,47 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  // signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  // useEffect(() => {
+  //   // declare the data fetching function
+  //   const fetchData = async () => {
+  //     const response = await getRedirectResult(auth);
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user);
+  //     }
+  //   };
+
+  //   // call the function
+  //   fetchData()
+  //     // make sure to catch any error
+  //     .catch(console.error);
+  // }, []);
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     //console.log(response);
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
+  // const logGoogleRedirect = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  //   //console.log(response);
+  //   const userDocRef = await createUserDocumentFromAuth(user);
+  // };
   return (
     <div>
       <h1>Sign In page </h1>
       <button onClick={logGoogleUser}>sign in with google popup</button>
+
+      {/* <button onClick={signInWithGoogleRedirect}>
+        sign in with google redirect
+      </button> */}
     </div>
   );
 };
